221f00e2768dd22755456a8aae7eab0c993fa7a3
Olimpai <olimpia.ropek@wp.pl>
2019-06-04 15:31:25 +0200

    (Prace z 11 maja, wcześniej wybrałam złą opcję i commitowałam na serwery bitbucket)

---
+++ Assets/Scenes/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush;
+    [SerializeField] float yPush;
+    [SerializeField] bool hasStarted = false;
+    [SerializeField] Rigidbody2D rb;
+     Vector2 paddleToBallVector; 
+
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.simulated = false;
+        
+    }
+
+  
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchtoPaddle();
+        }
+    }
+
+    private void LaunchtoPaddle ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            rb.simulated = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scenes/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenwidthinunits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenwidthinunits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+       
+    }
+}

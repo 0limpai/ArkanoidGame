6e9db4e00fc5118f7958c6bdac442d0692662638
Olimpai <olimpia.ropek@wp.pl>
2019-06-06 16:18:07 +0200

    Fixed some bugs, added button to start again game.

--- Assets/Scenes/Scripts/destroy.cs
+++ Assets/Scenes/Scripts/destroy.cs
@@ -6,23 +6,36 @@ public class destroy : MonoBehaviour
 {
    
     public Sprite BSR_2;
+    public Sprite BSR_3;
     public int hitsNeeded = 2;
     public int hitsTaken;
+    public Rigidbody2D ball;
+    private SpriteRenderer cc;
 
-   
-
+    void Start()
+    {
+        hitsTaken = 0;
+        cc = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        if (hitsTaken == 1)
+        { cc.sprite = BSR_2;
+        }
+        if (hitsTaken == 2)
+        {
+            cc.sprite = BSR_3;
+        }
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
 
         hitsTaken += 1;
-       if (hitsTaken >= 1)
+        if (hitsTaken >= hitsNeeded)
         {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = BSR_2;
-        }
-
-        else if (hitsTaken >= hitsNeeded)
             Destroy(gameObject);
+        }
 
 
     }
